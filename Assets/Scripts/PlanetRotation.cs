b6e79d0db427a8d96af6d20cb0e789c80f390641
Mário Pinto (BInteractive) <mario.pinto@binteractive.pt>
2015-01-07 12:08:26 +0000

    First Commit
    Textures
    Vuforia

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetRotation : MonoBehaviour {
+	Vector3 rotation = new Vector3(0,1,0);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(rotation);
+	}
+}
