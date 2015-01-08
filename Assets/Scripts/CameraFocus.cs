a397cf2418248f7e3cbdbf64722af9b9ebb57284
Mário Pinto (BInteractive) <mario.pinto@binteractive.pt>
2015-01-08 18:26:10 +0000

    PlanetsInitialization

---
+++ Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFocus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		bool focusModeSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		
+		if (!focusModeSet) {
+			Debug.Log("Failed to set focus mode (unsupported mode).");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
